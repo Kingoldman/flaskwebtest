980ea7b959b1bbf3afc5f8ed68eeaea1f314ce35
Kingoldman <44054434+Kingoldman@users.noreply.github.com>
2018-10-24 22:51:27 +0800

    Update config.py

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ class DevelopmentConfig(Config):
 
 	
 class ProductionConfig(Config):
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///'+os.path.join(basedir,'data.sqlite')
 	@classmethod
 	def init_app(cls,app):
 		Config.init_app(app)
